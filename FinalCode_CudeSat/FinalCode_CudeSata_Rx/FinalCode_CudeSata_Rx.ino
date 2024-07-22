7992eb1f93e1435a3883ba5a8c9243015367192e
Daniel19n33dy0u <lukasdaniel050403@gmail.com>
2024-07-22 09:20:09 +0700

    FinalCode CubeSat

---
+++ FinalCode_CudeSat/FinalCode_CudeSata_Rx/FinalCode_CudeSata_Rx.ino
@@ -0,0 +1,104 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial loraSerial(2, 3);
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define M0 11
+#define M1 10
+
+struct SensorData {
+  float temperature;
+  float pressure;
+  float altitude;
+  float latitude;
+  float longitude;
+  float uvIntensity;
+};
+
+SensorData sensor;
+
+unsigned long previousMillis = 0;
+const long interval = 2000; // Interval in milliseconds for updating display
+
+void setup() {
+  Serial.begin(115200);
+  loraSerial.begin(9600);
+  pinMode(M0, OUTPUT);
+  pinMode(M1, OUTPUT);
+  digitalWrite(M0, LOW);
+  digitalWrite(M1, LOW);
+  
+  // Initialize the OLED display with I2C address 0x3D
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+  display.display(); // Clear the buffer
+  display.clearDisplay();
+  
+  Serial.println("RECEIVER IS READY");
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (loraSerial.available() >= sizeof(sensor)) {
+    byte buffer[sizeof(sensor)];
+    for (int i = 0; i < sizeof(sensor); i++) {
+      buffer[i] = loraSerial.read();
+    }
+    memcpy(&sensor, buffer, sizeof(sensor));
+
+    // Display received sensor data
+    Serial.println("DATA RECEIVED");
+
+    Serial.print("Temperature RX : "); Serial.println(sensor.temperature);
+    Serial.print("Pressure RX    : "); Serial.println(sensor.pressure);
+    Serial.print("Altitude RX    : "); Serial.println(sensor.altitude);
+    Serial.print("Latitude RX    : "); Serial.println(sensor.latitude, 6);
+    Serial.print("Longitude RX   : "); Serial.println(sensor.longitude, 6);
+    Serial.print("UV Intensity RX: "); Serial.println(sensor.uvIntensity);
+    Serial.print("\n");
+
+    // Update OLED display every two seconds
+    if (currentMillis - previousMillis >= interval) {
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setTextColor(SSD1306_WHITE);
+      display.setCursor(0, 0);
+
+      display.print("T:");
+      display.print(sensor.temperature);
+      display.print("C");
+      display.print(" |");
+      display.print("P:");
+      display.print(sensor.pressure);
+      display.println("Pa");
+
+      display.print("A:");
+      display.print(sensor.altitude);
+      display.print("m");
+      display.print(" |");
+      display.print("UV:");
+      display.println(sensor.uvIntensity);
+      // display.println("uW/cm2");
+
+      display.print("Lat: ");
+      display.println(sensor.latitude, 6); 
+
+      display.print("Long: ");
+      display.println(sensor.longitude, 6); 
+
+      
+
+      display.display(); // Display the content
+      previousMillis = currentMillis;
+    }
+  }
+}
---
+++ FinalCode_CudeSat/FinalCode_CudeSata_Tx/FinalCode_CudeSata_Tx.ino
@@ -0,0 +1,142 @@
+#include <SoftwareSerial.h>
+#include "MS5611.h"
+#include <TinyGPS++.h>
+
+
+#define UV_SENSOR_PIN A0
+
+#define GPS_RX_PIN 4
+#define LORA_TX_PIN 3
+
+#define GPS_BAUD 9600
+// #define GPS_BAUD 115200
+
+#define M0 11
+#define M1 10
+
+// SoftwareSerial loraSerial(GPS_RX_PIN, LORA_TX_PIN);
+SoftwareSerial gpsLoraSerial(GPS_RX_PIN, LORA_TX_PIN);
+
+MS5611 TWO(0x77);
+TinyGPSPlus gps;
+
+struct SensorData {
+  float temperature;
+  float pressure;
+  float altitude;
+  float latitude;
+  float longitude;
+  float uvIntensity;
+};
+
+SensorData sensor;
+
+const int SENSOR_SAMPLE_TIME = 1000; // ms
+uint32_t t_sensor_now, t_sensor_last = 0;
+
+const int TX_INTERVAL = 10000; // ms
+uint32_t t_tx_now, t_tx_last = 0;
+
+void setup() {
+  Serial.begin(115200);
+  gpsLoraSerial.begin(GPS_BAUD);
+  // loraSerial.begin(9600);
+
+  pinMode(M0, OUTPUT);
+  pinMode(M1, OUTPUT);
+  digitalWrite(M0, 0);
+  digitalWrite(M1, 0);
+  
+  Wire.begin();
+  if (TWO.begin()) {
+    Serial.println("MS5611 0x77 found.");
+  } else {
+    Serial.println("MS5611 0x77 not found.");
+  }
+  Serial.println("TRANSMITTER IS READY");
+
+  t_sensor_now = t_sensor_last = millis();
+  t_tx_now = t_tx_last = millis();
+}
+
+void loop() {
+  t_sensor_now = millis();
+  t_tx_now = millis();
+
+  readGPSData();
+
+  if (t_sensor_now - t_sensor_last >= SENSOR_SAMPLE_TIME) {
+    readUVSensor();
+    readSensorData();
+    t_sensor_last = t_sensor_now;
+  }
+
+  if (t_tx_now - t_tx_last >= TX_INTERVAL) {
+    sendSensorData();
+    t_tx_last = t_tx_now;
+  }
+}
+
+void readUVSensor() {
+  int sensorValue = analogRead(UV_SENSOR_PIN);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  sensor.uvIntensity = voltage * 10.0;
+}
+
+        
+void readGPSData() {
+        // sensor.latitude = 11.485261;
+        // sensor.longitude = 104.872154;
+
+  while (gpsLoraSerial.available() > 0) {
+    if (gps.encode(gpsLoraSerial.read())) {
+      if (gps.location.isValid()) {
+        sensor.latitude = gps.location.lat();
+        sensor.longitude = gps.location.lng();
+      }
+
+      Serial.print("DATE : ");
+
+      if (gps.date.isValid())
+      {
+        Serial.print(gps.date.month());
+        Serial.print("/");
+        Serial.print(gps.date.day());
+        Serial.print("/");
+        Serial.println(gps.date.year());
+      }
+      else
+      {
+        Serial.println("Not Available");
+      }
+    }
+  }
+}
+
+void readSensorData() {
+  int result = TWO.read();
+  if (result == MS5611_READ_OK) {
+    sensor.temperature = TWO.getTemperature();
+    sensor.pressure = TWO.getPressure();
+    sensor.altitude = (1 - pow((sensor.pressure / 1013.25), 0.190284)) * 44330.8;
+  } else {
+    Serial.print("MS5611 Read Error: ");
+    Serial.println(result);
+  }
+}
+
+void sendSensorData() {
+  byte *dataBytes = (byte *)&sensor;
+  for (int i = 0; i < sizeof(sensor); i++) {
+    gpsLoraSerial.write(dataBytes[i]);
+  }
+  Serial.println("\nDATA SEND");
+
+  Serial.print("Temperature TX  : "); Serial.println(sensor.temperature);
+  Serial.print("Pressure TX     : "); Serial.println(sensor.pressure);
+  Serial.print("Altitude TX     : "); Serial.println(sensor.altitude);
+  Serial.print("Latitude TX     : "); Serial.println(sensor.latitude, 6);
+  Serial.print("Longitude TX    : "); Serial.println(sensor.longitude, 6);
+  Serial.print("UV Intensity TX : "); Serial.println(sensor.uvIntensity);
+  Serial.print("\n"); 
+}
